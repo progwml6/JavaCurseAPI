465adb4d814d844e89caf97a610a4adaf60f8290
Progwml6 <progwml6@gmail.com>
2016-04-27 19:47:51 -0400

    add gradle setup and sample request

---
+++ src/main/java/com/feed_the_beast/javacurselib/service/logins/login/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.feed_the_beast.javacurselib.service.logins.login;
+
+/**
+ * Created by progwml6 on 4/27/16.
+ */
+public class LoginRequest {
+    public String Username;
+    public String Password;
+}

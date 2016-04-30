7528d16cf023873208b961f251ed45303cbf8f79
Progwml6 <progwml6@gmail.com>
2016-04-30 13:57:19 -0400

    add Apis class that has a list of all the API urls

---
+++ src/main/java/com/feed_the_beast/javacurselib/Apis.java
@@ -0,0 +1,22 @@
+package com.feed_the_beast.javacurselib;
+
+/**
+ * list of known API's go to /help for API docs.
+ * websocket API's don't have docs you will need to go to the typescript samples for that information
+ */
+public class Apis {
+    public static final String ACCOUNTS = "https://accounts-v1.curseapp.net/";
+    public static final String AVATARS = "https://avatars.curseapp.net/";
+    public static final String CALLS = "https://calls-v1.curseapp.net/";
+    public static final String CONTACTS = "https://contacts-v1.curseapp.net/";
+    public static final String CONVERSATIONS = "https://conversations-v1.curseapp.net/";
+    public static final String FILES = "https://files.curseapp.net/";
+    public static final String GIVEAWAYS = "https://giveaways-v1.curseapp.net/";
+    public static final String GROUPS = "https://groups-v1.curseapp.net/";
+    public static final String IMAGES = "https://images.curseapp.net/";
+    public static final String LOGINS = "https://logins-v1.curseapp.net/";
+    public static final String NOTIFICATIONS = "https://notifications-v1.curseapp.net/";//This api is websocket based
+    public static final String POLLS = "https://polls-v1.curseapp.net/";
+    public static final String SESSIONS = "https://sessions-v1.curseapp.net/";
+    public static final String SYNC = "https://sync-v1.curseapp.net/";
+}
--- src/main/java/com/feed_the_beast/javacurselib/CurseApp.java
+++ src/main/java/com/feed_the_beast/javacurselib/CurseApp.java
@@ -10,37 +10,39 @@ import com.feed_the_beast.javacurselib.service.sessions.sessions.CreateSessionRe
 import com.feed_the_beast.javacurselib.service.sessions.sessions.DevicePlatform;
 import com.feed_the_beast.javacurselib.utils.NetworkRequest;
 
-import java.util.Scanner;
 import java.util.UUID;
 
 /**
  * Created by progwml6 on 4/28/16.
  */
 public class CurseApp {
-    public static LoginResponse login(String username, String password) {
+    public static LoginResponse login (String username, String password) {
         LoginRequest lr = new LoginRequest(username, password);
         String jsonToSend = JsonFactory.GSON.toJson(lr);
-        String s = NetworkRequest.postJson("https://logins-v1.curseapp.net/login", jsonToSend, null);
-        LoginResponse ret = JsonFactory.GSON.fromJson(s ,LoginResponse.class);
+        String s = NetworkRequest.postJson(Apis.LOGINS + "login", jsonToSend, null);
+        LoginResponse ret = JsonFactory.GSON.fromJson(s, LoginResponse.class);
         return ret;
     }
-    public static ContactsResponse getContacts(String token) {
-        String str = NetworkRequest.getJson("https://contacts-v1.curseapp.net/contacts", token);
+
+    public static ContactsResponse getContacts (String token) {
+        String str = NetworkRequest.getJson(Apis.CONTACTS + "contacts", token);
         return JsonFactory.GSON.fromJson(str, ContactsResponse.class);
     }
-    public static CreateSessionResponse getSession(String token, UUID machinekey, DevicePlatform platform) {
+
+    public static CreateSessionResponse getSession (String token, UUID machinekey, DevicePlatform platform) {
         String jsonToSend = JsonFactory.GSON.toJson(new CreateSessionRequest(machinekey, platform));
-        String str = NetworkRequest.postJson("https://sessions-v1.curseapp.net/sessions", jsonToSend, token);
+        String str = NetworkRequest.postJson(Apis.SESSIONS +"sessions", jsonToSend, token);
         return JsonFactory.GSON.fromJson(str, CreateSessionResponse.class);
     }
-    public static void main(String args[]) {
+
+    public static void main (String args[]) {
         LoginResponse lr = login(args[0], args[1]);
         ContactsResponse cr = getContacts(lr.Session.Token);
         CreateSessionResponse sessionResponse = getSession(lr.Session.Token, UUID.randomUUID(), DevicePlatform.UNKNOWN);
-        for(GroupNotification g : cr.Groups) {
-            if(g.GroupTitle.equals("CurseForge")){
+        for (GroupNotification g : cr.Groups) {
+            if (g.GroupTitle.equals("CurseForge")) {
                 for (ChannelContract c : g.Channels) {
-                    if(c.GroupTitle.equals("app-api-chat")) {
+                    if (c.GroupTitle.equals("app-api-chat")) {
                         System.out.println("you probably have access to this magical API Channel if you are seeing this code");
                     }
                 }

52e999a76577b056f2e2491dddebedddacc85aad
Progwml6 <progwml6@gmail.com>
2016-05-11 18:32:50 -0400

    remove busted account registration classes

--- src/main/java/com/feed_the_beast/javacurselib/service/logins/register/RegisterTempRequest.java
+++
@@ -1,9 +0,0 @@
-package com.feed_the_beast.javacurselib.service.logins.register;
-
-/**
- * representation of curse register api RegisterTempRequest
- * replies with RegisterTempResponse
- */
-public class RegisterTempRequest {
-    public String username;
-}

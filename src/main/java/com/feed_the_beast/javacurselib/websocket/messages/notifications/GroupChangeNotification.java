02f19b493fab26470552e40a7fe92556525711a6
Janne Kujanpää <jikuja@iki.fi>
2016-06-20 19:41:35 +0300

    Something minor

--- src/main/java/com/feed_the_beast/javacurselib/websocket/messages/notifications/GroupChangeNotification.java
+++ src/main/java/com/feed_the_beast/javacurselib/websocket/messages/notifications/GroupChangeNotification.java
@@ -4,9 +4,11 @@ import com.feed_the_beast.javacurselib.common.enums.GroupChangeType;
 import com.feed_the_beast.javacurselib.common.enums.GroupMemberRemovedReason;
 import com.feed_the_beast.javacurselib.common.classes.GroupMemberContract;
 import com.feed_the_beast.javacurselib.service.contacts.contacts.GroupNotification;
+import lombok.ToString;
 
 import java.util.Date;
 
+@ToString
 public class GroupChangeNotification extends BaseResponse implements Response {
     public GroupChangeType changeType;
     public long senderID;

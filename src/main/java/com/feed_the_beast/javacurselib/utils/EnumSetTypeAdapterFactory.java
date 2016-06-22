d7b2903a11c6575d7ffe10338b67933656deab9d
Janne Kujanpää <jikuja@iki.fi>
2016-06-23 00:29:07 +0300

    EnumSetTypeAdapterFactory changes
     * Use reflection to test Generic type parameter of Set
     * Might work

--- src/main/java/com/feed_the_beast/javacurselib/utils/EnumSetTypeAdapterFactory.java
+++ src/main/java/com/feed_the_beast/javacurselib/utils/EnumSetTypeAdapterFactory.java
@@ -10,9 +10,16 @@ import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.Set;
 
 public class EnumSetTypeAdapterFactory implements TypeAdapterFactory {
+
+    // TODO: add constructor and save allowed Enums
+
     @Override
     public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
         Class<T> rawType = (Class<T>) type.getRawType();
@@ -20,7 +27,36 @@ public class EnumSetTypeAdapterFactory implements TypeAdapterFactory {
             return null;
         }
 
-        // if unsupported enumType return null ?
+        System.out.println(rawType.getTypeName());
+        System.out.println(Arrays.toString(rawType.getTypeParameters()));
+        System.out.println(rawType.getName());
+        System.out.println(type);
+        Type type2 = type.getType();
+        Method m = null;
+        try {
+            m = type2.getClass().getDeclaredMethod("getActualTypeArguments", null);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+
+        m.setAccessible(true);
+
+        Type[] ret = null;
+        try {
+            ret = (Type[]) m.invoke(type2);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Arrays.toString(ret));
+
+        if (ret[0] != GroupPermissions.class) {
+            // todo: check if this particular enum is in allowed list
+            return null;
+        }
 
         return (TypeAdapter<T>) new EnumSetTypeAdapter(type);
         //return new EnumSetTypeAdapter();

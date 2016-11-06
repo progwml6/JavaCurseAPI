774be33bd73676df0bc70cef08da48eac909e0f2
Progwml6 <progwml6@gmail.com>
2016-11-06 14:08:22 -0500

    Add chat formatting helper
    * TODO add support for @ tagging users in here

---
+++ src/main/java/com/feed_the_beast/javacurselib/utils/ChatFormatter.java
@@ -0,0 +1,38 @@
+package com.feed_the_beast.javacurselib.utils;
+
+/**
+ * see https://support.curse.com/hc/en-us/articles/210356803-Text-Chat-Message-Formatting
+ */
+public class ChatFormatter {
+    //TODO add support for @ tagging users
+
+    public static String bold (String in) {
+        return "*" + in + "*";
+    }
+
+    public static String italics (String in) {
+        return "~" + in + "~";
+    }
+
+    public static String underscore (String in) {
+        return "_" + in + "_";
+    }
+
+    public static String strikethrough (String in) {
+        return "-" + in + "-";
+    }
+
+    public static String monospace (String in) {
+        return "`" + in + "`";
+    }
+
+    /**
+     * Adds multiline monospacing -- newlines added automatically
+     * @param in String to format
+     * @return Formatted string
+     */
+    public static String mulitlineMonospace (String in) {
+        return "```\n" + in + "\n```";
+    }
+
+}
